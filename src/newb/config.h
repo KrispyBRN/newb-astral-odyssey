956648d04da5e6021f8b4d71335d18278697dedf
KrispyBRN <rerekrek16@gmail.com>
2026-08-14 15:50:41 +0700

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -138,17 +138,17 @@
 #define NL_CLOUD1_OPACITY 0.9              // 0.0 invisible ~ 1.0 opaque
 
 /* Rounded cloud settings */
-#define NL_CLOUD2_THICKNESS 3.0             // 0.5 slim ~ 5.0 fat
-#define NL_CLOUD2_RAIN_THICKNESS 2.5       // 0.5 slim ~ 5.0 fat
-#define NL_CLOUD2_STEPS 15                 // 3 low quality ~ 16 high quality
-#define NL_CLOUD2_SCALE vec2(0.012, 0.012) // 0.003 large ~ 0.3 tiny
+#define NL_CLOUD2_THICKNESS 4.0            // 0.5 slim ~ 5.0 fat
+#define NL_CLOUD2_RAIN_THICKNESS 5.0       // 0.5 slim ~ 5.0 fat
+#define NL_CLOUD2_STEPS 10                 // 3 low quality ~ 16 high quality
+#define NL_CLOUD2_SCALE vec2(0.005, 0.005) // 0.003 large ~ 0.3 tiny
 #define NL_CLOUD2_SHAPE vec2(0.5, 0.4)     // 0.0 round ~ 1.0 box vec2(horizontal shape, vertical shape)
 #define NL_CLOUD2_DENSITY 25.0             // 1.0 blurry ~ 100.0 sharp
 #define NL_CLOUD2_VELOCITY 0.8             // 0.0 static ~ 4.0 very fast
 //#define NL_CLOUD2_LAYER2                      // [toggle] extra cloud layer
 #define NL_CLOUD2_LAYER2_OFFSET 143.0           // 30.0 near ~ 300.0 very high
-#define NL_CLOUD2_LAYER2_THICKNESS 2.5          // 0.7 slim ~ 5.0 fat
-#define NL_CLOUD2_LAYER2_RAIN_THICKNESS 3.0     // 0.7 slim ~ 5.0 fat
+#define NL_CLOUD2_LAYER2_THICKNESS 3.5          // 0.7 slim ~ 5.0 fat
+#define NL_CLOUD2_LAYER2_RAIN_THICKNESS 3.5     // 0.7 slim ~ 5.0 fat
 #define NL_CLOUD2_LAYER2_STEPS 3                // 3 low quality ~ 16 high quality
 #define NL_CLOUD2_LAYER2_SCALE vec2(0.03, 0.03) // 0.003 large ~ 0.3 tiny
 #define NL_CLOUD2_LAYER2_SHAPE vec2(0.5, 0.4)   // 0.0 round ~ 1.0 box vec2(horizontal shape, vertical shape)
